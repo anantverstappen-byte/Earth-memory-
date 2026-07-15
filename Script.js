131fb61a167e539691acc7ff1894ac722e5a76d5
anantverstappen-byte <anantverstappen@gmail.com>
2026-07-15 19:48:32 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,19 @@
+function changeData(){
+
+document.getElementById("temp").innerHTML="33.4 °C";
+
+document.getElementById("humidity").innerHTML="61 %";
+
+document.getElementById("air").innerHTML="Moderate";
+
+document.getElementById("noise").innerHTML="58 dB";
+
+document.getElementById("light").innerHTML="650 Lux";
+
+document.getElementById("soil").innerHTML="64 %";
+
+document.getElementById("score").innerHTML="78";
+
+document.getElementById("insight").innerHTML="⚠ Air Quality Dropping. Noise Increasing.";
+
+}
